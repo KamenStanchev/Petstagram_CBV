2fed25f9e7f363320047ea9f26251ba707bdd9c0
Kamen Stanchev <k.s@abv.bg>
2022-07-30 08:35:31 +0300

    29.07

--- Petstagram/accounts/urls.py
+++ Petstagram/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from Petstagram.accounts.views import create_accout, login_page, account_details, edit_profile, edit_password
+from Petstagram.accounts.views import create_accout, login_page, account_details, edit_profile, edit_password, logout_page
 
 urlpatterns = [
     path('create-profile/', create_accout, name='create_account'),
-    path('login/', login_page, name='login'),
+    path('login/', login_page, name='login_page'),
+    path('logout/', logout_page, name='logout_page'),
     path('profile/<int:pk>/', account_details, name='account_detail'),
     path('edit-profile/<int:pk>/', edit_profile, name='edit_account'),
     path('edit-password/<int:pk>/', edit_password, name='edit_password'),
--- Petstagram/accounts/views.py
+++ Petstagram/accounts/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.contrib.auth import forms
+from django.contrib.auth import forms, authenticate, login, logout
 
 
 def create_accout(request):
@@ -9,15 +10,35 @@ def create_accout(request):
         form = forms.UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home_page')
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created with username: ' + user)
+            return redirect('login_page')
 
-    context = {'form': form,}
+    context = {'form': form, }
     return render(request, 'create_account.html', context)
 
 
-def login_page():
-    pass
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'User was successful log-in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'User or passwowrd is invalid')
 
+    form = forms.AuthenticationForm
+    context = {'form': form, }
+    return render(request, 'login_page.html', context)
+
+
+def logout_page(request):
+    logout(request)
+    messages.success(request, 'User was successful LogOut')
+    return redirect('login_page')
 
 def account_details():
     pass
