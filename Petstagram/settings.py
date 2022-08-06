0539ccb819d84218f093c307578cfcf215359ee2
Kamen Stanchev <k.s@abv.bg>
2022-08-06 17:42:12 +0300

    heroku settings

--- Petstagram/settings.py
+++ Petstagram/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^in1%dn-$r80*y5g-1@q6-wd7u3d&$p01o-aav*qj^^%v_*ho('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kamen-petstagram.herokuapp.com/', '127.0.0.1']
 
 # Application definition
 
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'Petstagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petstagram_db',
+        'USER': 'postgres',
+        'PASSWORD': '1123QwER',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Petstagram.wsgi --log-file -
