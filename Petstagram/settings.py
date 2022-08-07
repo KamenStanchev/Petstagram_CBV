a7bae3b282420a988ea3ed46d4446229743849a9
Kamen Stanchev <k.s@abv.bg>
2022-08-07 16:50:29 +0300

    staticfile setup in settings.py

--- Petstagram/settings.py
+++ Petstagram/settings.py
@@ -145,11 +145,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
