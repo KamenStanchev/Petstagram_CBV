4553b290dd485b0e32e4a3b7f2310d0e92b8408f
Kamen Stanchev <k.s@abv.bg>
2022-08-07 10:34:57 +0300

    fix urlpattern for account_list

--- Petstagram/accounts/urls.py
+++ Petstagram/accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', login_page, name='login_page'),
     path('logout/', logout_page, name='logout_page'),
     path('account-details/', account_details, name='account_detail'),
-    path('accounts-list/', AccountsListView.as_view, name='accounts_list'),
+    path('accounts-list/', AccountsListView.as_view(), name='accounts_list'),
     path('edit-account/', edit_account, name='edit_account'),
     path('edit-password/', edit_password, name='edit_password'),
 
